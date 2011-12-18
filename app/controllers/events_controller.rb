41690b93076f88262a0575b234bd0e142721fe90
Rainer Blessing <rainer.blessing@googlemail.com>
2011-12-18 21:26:05 +0100

    create five movie choices

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,7 +26,7 @@ class EventsController < ApplicationController
   # GET /events/new.json
   def new
     @event = Event.new
-    3.times {@event.movies.build }
+    5.times {@event.movies.build }
 
     respond_to do |format|
       format.html # new.html.erb
