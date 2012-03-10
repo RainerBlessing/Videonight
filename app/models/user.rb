5da1d3a33ac9b11dbb0c3a3ad5ae5ea0270a1b6c
Rainer Blessing <rainer.blessing@googlemail.com>
2012-03-10 11:58:40 +0100

    externalized ldap configuration, improved event view

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,13 @@ class User < ActiveRecord::Base
 
   ajaxful_rater
 
-  ### BEGIN CONFIGURATION ###
-  SERVER = '10.6.2.240'         # Active Directory server name or IP
-  PORT = 389                    # Active Directory server port (default 389)
-  BASE = 'DC=intershopnet'    # Base to search from
-  DOMAIN = 'intershopnet'        # For simplified user@domain format login
-  ### END CONFIGURATION ###
-
   def self.authenticate(login, password)
    return false if login.empty? or password.empty?
  
-    conn = Net::LDAP.new :host => SERVER,
-                         :port => PORT,
-                         :base => BASE,
-                         :auth => { :username => "#{login}@#{DOMAIN}",
+    conn = Net::LDAP.new :host => APP_CONFIG['server'],
+                         :port => APP_CONFIG['port'],
+                         :base => APP_CONFIG['base'],
+                         :auth => { :username => "#{login}@#{APP_CONFIG['domain']}",
                                     :password => password,
                                     :method => :simple }
     if conn.bind
