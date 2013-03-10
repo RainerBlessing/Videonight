89d16f5cb348f9831892feba68084e7f541ca7af
Rainer Blessing <rainer.blessing@googlemail.com>
2013-03-10 21:17:02 +0100

    moved sorting into model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,8 @@ class Event < ActiveRecord::Base
   has_many :movies, :through => :selections
 
   accepts_nested_attributes_for :selections, :movies, :allow_destroy => true
+  
+  def sorted_selections
+    selections.sort!{|a,b| -(a.rate_average(false,:preference) <=> b.rate_average(false,:preference))}
+  end
 end
